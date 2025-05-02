0c2233bcc5a9588efb18d9710d7edc5f65f8abe8
AnantK2709 <anant009@e.ntu.edu.sg>
2025-05-02 21:50:02 +0800

    Null values

--- flask/getpost.py
+++ flask/getpost.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -26,6 +26,12 @@ def submit():
         email = request.form.get("email")
         return f'Name: {name}, Email: {email}'
     return render_template("form.html")
+
+@app.route("/items", methods=["GET"])
+def items():
+    items = ["Item 1", "Item 2", "Item 3"]
+    return jsonify(items)
+
 
 
 if __name__ == "__main__":
